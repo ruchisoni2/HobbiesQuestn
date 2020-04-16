a82ccf52af0a0b9ad985c4ad4c3d36ed2dd5f6f4
Ruchi Soni <r.soni708@gmail.com>
2020-04-17 00:16:19 +0530

    angular

---
+++ src/app/hobbies.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-hobbies',
+    templateUrl: './hobbies.component.html',
+    styleUrls: ['./hobbies.component.css']
+})
+
+export class HobbiesComponent implements OnInit{
+
+    data = [];
+    constructor(){}
+    ngOnInit(): void{}
+
+    store(newValue:string){
+        this.data.push(newValue.split(","));
+        console.log(this.data);
+    }
+}
